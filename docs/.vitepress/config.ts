70fc536fe48b4c1543f556469763f140da7f51c5
Akayy <techakayy@gmail.com>
2023-08-26 13:26:57 +1000

    Updated to use srcDir

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   /* Uncomment this line */
-  // srcDir: 'theme/pages',
+  srcDir: 'theme/pages',
   title: 'My Awesome Project',
   description: 'A VitePress Site',
   themeConfig: {
